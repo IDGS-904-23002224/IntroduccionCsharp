49a663ec174e68bbd0387a654114e1ef2e826904
Axel Rocha <85525@alumnos.utleon.edu.mx>
2026-04-30 23:39:20 -0600

    Práctica 1.1

---
+++ IntroduccionCsharp/Controllers/PruebaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionCsharp.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Suma(String x, String y, String r1)
+        {
+            int res = Convert.ToInt16(x) + Convert.ToInt16(y);
+
+            ViewBag.Res = Convert.ToString(res);
+
+            return View();
+        }
+    }
+}
